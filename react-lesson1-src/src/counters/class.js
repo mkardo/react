d65cf2fd8b2693e836008f23e37c99e5165070b5
Marina Kardopolova <mkardopolova@lineate.com>
2019-08-02 18:00:24 +0600

    Right homepage #1

--- react-lesson1-src/src/counters/class.js
+++ react-lesson1-src/src/counters/class.js
@@ -1,72 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class extends React.Component{
 
+    static propTypes = {
+        min: PropTypes.number.isRequired,
+        max: PropTypes.number.isRequired
+    }
+
     state = {
         cnt: this.props.min,
-        alert: ''
+        inputValue: this.props.min
     };
 
     increase = () => {
-        if (this.state.cnt < this.props.max) {
-            this.setState({
-                cnt: this.state.cnt + 1,
-                alert: ''
-            });
-        }
+        this.setCnt(this.state.cnt + 1);
     }
 
     decrease = () => {
-        if (this.state.cnt > this.props.min) {
-            this.setState({
-                cnt: this.state.cnt - 1,
-                alert: ''
-            });
-        }
+        this.setCnt(this.state.cnt - 1);
     }
 
-    handleChange = (event) => {
-
-        const value = +event.target.value;
-
-        if (isNaN(value)) {
-            this.setState({
-                cnt: event.target.value,
-                alert: 'Введено неверное значение'
-            });
-
-            return;
-        }
-
-        if (value > this.props.max) {
-            this.setState({
-                cnt: event.target.value,
-                alert: 'Число больше максимума'
-            });
-
-            return;
-        }
-
-        if (value < this.props.min) {
-            this.setState({
-                cnt: event.target.value,
-                alert: 'Число меньше минимума'
-            });
-            
-            return;
-        }
-
+    setCnt(newCnt) {
+        let cnt = Math.min(Math.max(newCnt, this.props.min), this.props.max);
         this.setState({
-            cnt: value,
-            alert: ''
+            cnt,
+            inputValue: cnt
         });
+    }
+
+    handleChange(newStr) {
+        /*let cnt = parseInt(newStr);
+
+        this.setCnt(isNaN(cnt) ? this.props.min : cnt);*/
+        this.setState({
+            inputValue: newStr
+        });
+    }
+
+    applyValue = () => {
+        let cnt = parseInt(this.state.inputValue);
+
+        this.setCnt(isNaN(cnt) ? this.props.min : cnt);
+    }
+
+    checkEnterKey = (e) => {
+        if (e.keyCode === 13) {
+            this.applyValue();
+        }
     }
 
     render(){
         return (
             <div>
                 <button onClick={this.decrease}>Minus 1</button>
-                <input type="text" value={this.state.cnt} onChange={this.handleChange}/>
+                <input
+                    type="text"
+                    value={this.state.inputValue}
+                    onChange={(e) => this.handleChange(e.target.value)}
+                    onBlur={this.applyValue}
+                    onKeyUp={this.checkEnterKey}
+                />
                 <button onClick={this.increase}>Plus 1</button>
                 <div>
                     <span>{this.state.alert}</span>
